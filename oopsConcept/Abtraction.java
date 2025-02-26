e669864e17c720aefcf7e76de7e22e64a8b55733
SanketB2004 <bhosalesanket1507@gmail.com>
2025-02-26 06:51:49 +0530

    Make

--- oopsConcept/Abtraction.java
+++ oopsConcept/Abtraction.java
@@ -1,15 +1,27 @@
 
+// Abstract Class
 abstract class InnerAbtraction {
-
-    public abstract void method();
+    public abstract void method();  // Abstract method
 }
 
- interface InnerAbtraction_1 {
-void method();
-    
+// Interface
+interface InnerAbtraction_1 {
+    void method();  // Interface method (default public & abstract)
 }
+
+// Subclass (Abstract class extend करत आहे आणि Interface implement करत आहे)
+class SubClass extends InnerAbtraction implements InnerAbtraction_1 {
+    // method() चं implementation द्यावं लागेल
+    public void method() {
+        System.out.println("Abstract class आणि Interface चे method Override केले");
+    }
+}
+
+// Main Class
 public class Abtraction {
-public static void main(String[] args) {
-    
+    public static void main(String[] args) {
+        SubClass obj = new SubClass();  // SubClass चे object तयार केले
+        obj.method();  // method() कॉल केला
+    }
 }
-}
+
